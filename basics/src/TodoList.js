4f0169bf78902315314e7ec6e8d37795a0c9335d
uma <umaraaj1999@gmail.com>
2024-11-27 11:52:21 +0530

    created app file

---
+++ basics/src/TodoList.js
@@ -0,0 +1,19 @@
+import { TodoKeys } from "./TodoKeys"
+
+export function TodoList({ todos, toggleTodo, deleteTodo }) {
+  return (
+    <ul className="list">
+      {todos.length === 0 && "No Todos"}
+      {todos.map(todo => {
+        return (
+          <TodoKeys
+            {...todo}
+            key={todo.id}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        )
+      })}
+    </ul>
+  )
+}
